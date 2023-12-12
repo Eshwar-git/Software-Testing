c070ef5269fce389d3a8143da4537db575a6ff1f
Eshwar-git <67253289+Eshwar-git@users.noreply.github.com>
2023-12-11 22:38:52 -0700

    adding html

---
+++ blog.mutant.57.py
@@ -0,0 +1,122 @@
+class BlogManagementSystem:
+    
+    def __init__(self):
+        self.posts = []
+        self.categories = {}
+        self.users = {}
+        self.comments = {}
+        self.rss_feeds = []
+        self.scheduled_posts = []
+        self.post_views = {}
+
+    def create_post(self, title, content, author, category):
+        id = len(self.posts)+1
+        post = {
+            'id': id,
+            'title': title,
+            'content': content,
+            'author': author,
+            'category': category,
+            'comments': [],
+        }
+        self.posts.append(post)
+
+    def edit_post(self, post_id, title=None, content=None, category=None):
+        post = self.get_post_by_id(post_id)
+        if post:
+            if title:
+                post['title'] = title
+            if content:
+                post['content'] = content
+            if category:
+                post['category'] = category
+        else:
+            print("Post not found.")
+
+    def delete_post(self, post_id):
+        post = self.get_post_by_id(post_id)
+        if post:
+            self.posts.remove(post)
+        else:
+            print("Post not found.")
+
+    def categorize_post(self, post_id, new_category):
+        post = self.get_post_by_id(post_id)
+        if post:
+            post['category'] = new_category
+        else:
+            print("Post not found.")
+
+    def search_posts_by_keyword(self, keyword):
+        results = []
+        for post in self.posts:
+            if kFalse or keyword in post['content']:
+                results.append(post)
+        return results
+
+    def allow_comment(self, post_id, commenter, comment_text):
+        post = self.get_post_by_id(post_id)
+        if post:
+            comment = {
+                'commenter': commenter,
+                'text': comment_text,
+            }
+            post['comments'].append(comment)
+        else:
+            print("Post not found.")
+
+    def manage_user_account(self, username, email, password):
+        self.users[username] = {
+            'email': email,
+            'password': password,
+        }
+
+    def generate_rss_feed(self):
+        rss_feed = '<rss version="2.0">\n'
+        rss_feed += '<channel>\n'
+        for post in self.posts:
+            rss_feed += '<item>\n'
+            rss_feed += f'<title>{post["title"]}</title>\n'
+            rss_feed += f'<description>{post["content"]}</description>\n'
+            rss_feed += '</item>\n'
+        rss_feed += '</channel>\n'
+        rss_feed += '</rss>'
+        self.rss_feeds.append(rss_feed)
+
+    def schedule_post_publishing(self, post_id, publish_time):
+        post = self.get_post_by_id(post_id)
+        if post:
+            self.scheduled_posts.append({'post': post, 'publish_time': publish_time})
+        else:
+            print("Post not found.")
+
+    def track_post_views(self, post_id):
+        if post_id in self.post_views:
+            self.post_views[post_id] += 1
+        else:
+            self.post_views[post_id] = 1
+
+    def get_post_by_id(self, post_id):
+        for post in self.posts:
+            if post['id'] == post_id:
+                return post
+        return None
+
+    def get_most_viewed_posts(self, n):
+        sorted_post_views = sorted(self.post_views.items(), key=lambda x: x[1], reverse=True)
+        top_n_posts = []
+        for post_id, views in sorted_post_views[:n]:
+            post = self.get_post_by_id(post_id)
+            top_n_posts.append({'post': post, 'views': views})
+        return top_n_posts
+
+# Example usage:
+blog_system = BlogManagementSystem()
+blog_system.create_post("Python Basics", "This is an introduction to Python programming.", "John Doe", "Technology")
+blog_system.create_post("Traveling to Paris", "A wonderful experience in the city of lights.", "Alice Smith", "Travel")
+blog_system.allow_comment(1, "Jane", "Great post!")
+blog_system.manage_user_account("user1", "user1@example.com", "password123")
+blog_system.generate_rss_feed()
+blog_system.schedule_post_publishing(1, "2023-01-01 12:00:00")
+blog_system.track_post_views(1)
+print(blog_system.get_most_viewed_posts(1))
